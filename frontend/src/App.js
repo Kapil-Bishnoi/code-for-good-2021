35445642751bc71cd94461851bf30a02e03defe0
KAPIL <kapil992000@gmail.com>
2021-06-25 18:11:04 +0530

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from "react";
+import React, { useContext } from "react";
 import "./CSS/App.css";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { Header } from "./components/Header";
@@ -7,20 +7,13 @@ import { SignupEvaluator } from "./components/SignupEvaluator";
 import { SignupMentor } from "./components/SignupMentor";
 import { SignupStudent } from "./components/SignupStudent";
 import { Login } from "./components/Login";
-import {AuthContext} from './context/Auth';
-import {fire} from './firebase';
-import axios from "axios";
+import { AuthContext } from "./context/Auth";
+import { HeaderAuth } from "./components/HeaderAuth";
 
 function App() {
+	const { firebaseUser } = useContext(AuthContext);
 
-	const {firebaseUser} = useContext(AuthContext);
-
-	const firebaseLogout = () => {
-		fire.auth().signOut();
-		localStorage.removeItem("userId");
-	}
-
-	if(!firebaseUser){
+	if (!firebaseUser) {
 		return (
 			<div className="App">
 				<Header />
@@ -31,7 +24,11 @@ function App() {
 						path="/signup/student"
 						component={() => <SignupStudent />}
 					/>
-					<Route exact path="/signup/mentor" component={() => <SignupMentor />} />
+					<Route
+						exact
+						path="/signup/mentor"
+						component={() => <SignupMentor />}
+					/>
 					<Route
 						exact
 						path="/signup/evaluator"
@@ -42,12 +39,17 @@ function App() {
 				</Switch>
 			</div>
 		);
-	}
-	else{
+	} else {
 		return (
-			<div>
-				<h1>Welcome!</h1>
-				<button onClick={() => firebaseLogout()}>Logout</button>
+			<div className="App">
+				<HeaderAuth />
+				<Switch>
+					<Route exact path="/home" component={() => <Home />} />
+					<Route exact path="/createproject" component={() => <Home />} />
+					<Route exact path="/projects" component={() => <Home />} />
+					<Route exact path="/profile" component={() => <Home />} />
+					<Redirect to="/home" />
+				</Switch>
 			</div>
 		);
 	}
---
+++ frontend/src/components/HeaderAuth.js
@@ -0,0 +1,153 @@
+import React from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import { Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import { fire } from "../firebase";
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	left: {
+		flexGrow: 1,
+	},
+	logo: {
+		height: "50px",
+		cursor: "pointer",
+	},
+	headerOptions: {
+		display: "flex",
+		justifyContent: "space-evenly",
+	},
+	headerItem: {
+		margin: "10px",
+	},
+}));
+
+export const HeaderAuth = () => {
+	const classes = useStyles();
+	const [optionSwitch, toggleOptionSwitch] = React.useState(null);
+	const logoURL =
+		"https://challenge.balajanaagraha.org/_nuxt/static/bja-html-ui/assets/img/LogoHorizontal.png";
+	const theme = useTheme();
+	const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+	const history = useHistory();
+
+	const handleMenu = (event) => {
+		toggleOptionSwitch(event.currentTarget);
+	};
+
+	const handleButtonClick = (pathURL) => {
+		history.push(pathURL);
+		toggleOptionSwitch(null);
+	};
+
+    const firebaseLogout = () => {
+		fire.auth().signOut();
+		localStorage.removeItem("userId");
+	}
+
+	return (
+		<div className={classes.root}>
+			<AppBar position="static">
+				<Toolbar>
+					<div className={classes.left}>
+						<img className={classes.logo} src={logoURL} alt="logo" />
+					</div>
+					{isMobile ? (
+						<div>
+							<IconButton
+								edge="start"
+								className={classes.menuButton}
+								color="inherit"
+								aria-label="menu"
+								onClick={handleMenu}
+							>
+								<MenuIcon />
+							</IconButton>
+							<Menu
+								id="menu-appbar"
+								anchorEl={optionSwitch}
+								anchorOrigin={{
+									vertical: "top",
+									horizontal: "right",
+								}}
+								keepMounted
+								transformOrigin={{
+									vertical: "top",
+									horizontal: "right",
+								}}
+								open={Boolean(optionSwitch)}
+								onClose={() => toggleOptionSwitch(null)}
+							>
+								<MenuItem onClick={() => handleButtonClick("/home")}>
+									Home
+								</MenuItem>
+								<MenuItem onClick={() => handleButtonClick("/createproject")}>
+									Create/Join Project
+								</MenuItem>
+								<MenuItem onClick={() => handleButtonClick("/projects")}>
+									Projects
+								</MenuItem>
+                                <MenuItem onClick={() => handleButtonClick("/profile")}>
+									Profile
+								</MenuItem>
+                                <MenuItem onClick={() => firebaseLogout()}>
+									Logout
+								</MenuItem>
+							</Menu>
+						</div>
+					) : (
+						<div className={classes.headerOptions}>
+							<Button
+								variant="contained"
+								onClick={() => handleButtonClick("/home")}
+								className={classes.headerItem}
+							>
+								HOME
+							</Button>
+							<Button
+								variant="contained"
+								onClick={() => handleButtonClick("/createproject")}
+								className={classes.headerItem}
+							>
+								Create/Join Project
+							</Button>
+                            <Button
+								variant="contained"
+								onClick={() => handleButtonClick("/projects")}
+								className={classes.headerItem}
+							>
+								Projects
+							</Button>
+                            <Button
+								variant="contained"
+								onClick={() => handleButtonClick("/profile")}
+								className={classes.headerItem}
+							>
+								Profile
+							</Button>
+							<Button
+								variant="contained"
+								onClick={() => firebaseLogout()}
+								className={classes.headerItem}
+							>
+								Logout
+							</Button>
+						</div>
+					)}
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
+};
