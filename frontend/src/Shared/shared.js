430254be9a8d6cbb1879a53571cca88e3198ba9f
KAPIL <kapil992000@gmail.com>
2021-07-03 00:21:31 +0530

    added home page

---
+++ frontend/src/Shared/shared.js
@@ -0,0 +1,60 @@
+export const carouselImages = [
+    {
+        id: 0,
+        image:
+            "https://challenge.balajanaagraha.org/_nuxt/static/bja-html-ui/assets/img/about1.jpg",
+    },
+    {
+        id: 1,
+        image:
+            "https://challenge.balajanaagraha.org/_nuxt/static/bja-html-ui/assets/img/about2.jpg",
+    },
+    {
+        id: 2,
+        image:
+            "https://challenge.balajanaagraha.org/_nuxt/static/bja-html-ui/assets/img/about3.jpg",
+    },
+    {
+        id: 3,
+        image:
+            "https://challenge.balajanaagraha.org/_nuxt/static/bja-html-ui/assets/img/about4.jpg",
+    },
+];
+
+export const fiveSteps = [
+    {
+        id: 1,
+        info: " Identify an issue that bothers you in you locality . You can pick among these: Road Safety, Waste Management, Climet Change, Water and Electricity, Conservation, COVID-19 or Other issues. Form a team or work as a solo warrior!",
+        label: "Identify",
+        image:
+            "https://media.istockphoto.com/photos/identity-problem-picture-id172442753",
+    },
+    {
+        id: 2,
+        info: "Find out everything you can about the issue you have chosen.What are the deeper causes for this issue? Speak to your family, friends, people in your neighbourhood or member of local governance to learn more",
+        label: "Investigate",
+        image:
+            "https://research.phoenix.edu/sites/default/files/blogpost/images/identify-research-problem-hero.jpg",
+    },
+    {
+        id: 3,
+        info: "  Now that you know more about the issue, what are the solutions for the issue? Write down a plan of action to solve your issue. Note that your solution has to last!",
+        label: "Ideation",
+        image:
+            "https://public-media.interaction-design.org/images/ux-daily/583881aca2fd2.jpg",
+    },
+    {
+        id: 4,
+        info: "  Now it’s time to act! Implement your solution. Seek help of who you think can support you and reach out to them!",
+        label: "Implementation",
+        image:
+            "https://image.shutterstock.com/image-vector/implementation-single-word-banner-concept-260nw-1482141995.jpg",
+    },
+    {
+        id: 5,
+        info: "Now that you have implemented your solution, let the world know! Submit your project and inspire others to bring about change in their local communities!",
+        label: "Inform",
+        image:
+            "https://scontent.fjai1-2.fna.fbcdn.net/v/t1.6435-9/102666535_110942493984318_870942647616341081_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=e3f864&_nc_ohc=bnbG9L-lBU4AX-oGEOj&_nc_ht=scontent.fjai1-2.fna&oh=e24b229b02c3c723d98af67de4d095dc&oe=60E44851",
+    },
+];
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,174 @@
-import React from 'react'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+	Container,
+	Grid,
+	Typography,
+	Card,
+	CardActionArea,
+	CardMedia,
+	CardContent,
+} from "@material-ui/core";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import KeyboardArrowLeftIcon from "@material-ui/icons/KeyboardArrowLeft";
+import Carousel from "react-material-ui-carousel";
+import { carouselImages, fiveSteps } from "../Shared/shared";
+
+const useStyles = makeStyles((theme) => ({
+	home_page: {
+		display: "flex",
+		flexDirection: "column",
+		marginTop: theme.spacing(6),
+	},
+	homepage_top: {
+		display: "flex",
+		[theme.breakpoints.down("sm")]: {
+			flexDirection: "column-reverse",
+			alignItems: "center",
+		},
+		[theme.breakpoints.up("md")]: {
+			flexDirection: "row",
+			alignItems: "center",
+		},
+	},
+	carousel_side: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	carousel_image: {
+		height: "450px",
+		width: "650px",
+	},
+	about_fivei: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		flexWrap: "wrap",
+		marginBottom: theme.spacing(6),
+		padding: theme.spacing(2),
+		backgroundColor: "#eee3e8",
+	},
+	fivei_card: {
+		margin: theme.spacing(2),
+		width: "280px",
+		height: "320px",
+	},
+	card_media: {
+		height: "110px",
+	},
+	fivei_heading: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		marginTop: theme.spacing(6),
+	},
+}));
 
 export const Home = () => {
-    return (
-        <div className="home">
-            <h1>Home Page</h1>
-        </div>
-    )
-}
+	const classes = useStyles();
+
+	return (
+		<Container disableGutters className={classes.home_page}>
+			<Container className={classes.homepage_top}>
+				<Grid xs={12} md={5} item className={classes.about_side}>
+					<Typography component="h1" variant="h3" style={{ color: "#5c68d6" }}>
+						About The Competiton
+					</Typography>
+					<Typography
+						component="h3"
+						variant="h4"
+						style={{
+							color: "#d0007b",
+							fontWeight: "bold",
+							fontFamily: "Comic Sans MS",
+						}}
+					>
+						Ready Set Go!
+					</Typography>
+					<Typography
+						component="h6"
+						variant="h6"
+						style={{
+							color: "#000000",
+							fontFamily: "Comic Sans MS",
+							fontWeight: "bold",
+							marginTop: "25px",
+						}}
+					>
+						Our City, Our Challenge is inviting youth to demonstrate how your
+						act of Active Citizenship can potentially influence quality of life.
+					</Typography>
+					<Typography
+						component="h6"
+						variant="h6"
+						style={{
+							color: "#434343",
+							marginTop: "25px",
+						}}
+					>
+						Active Citizenship actions by you, will go a long way in improving
+						our local communities, our cities and therefore, our country. The
+						Challenge enables 21st century skills in you! Share your innovative
+						ideas and actions with us!
+					</Typography>
+				</Grid>
+				<Grid xs={12} md={7} item className={classes.carousel_side}>
+					<Carousel
+						NextIcon={<KeyboardArrowRightIcon />}
+						PrevIcon={<KeyboardArrowLeftIcon />}
+					>
+						{carouselImages.map((cimg) => (
+							<img
+								key={cimg.id}
+								src={cimg.image}
+								className={classes.carousel_image}
+							/>
+						))}
+					</Carousel>
+				</Grid>
+			</Container>
+			<Grid item className={classes.fivei_heading}>
+				<Typography
+					component="h3"
+					variant="h4"
+					style={{
+						color: "#f186c0",
+						fontWeight: "bold",
+						fontFamily: "Comic Sans MS",
+					}}
+				>
+					5 Steps of the Challenge
+				</Typography>
+			</Grid>
+			<Container className={classes.about_fivei}>
+				{fiveSteps.map((step) => {
+					return <AboutCard key={step.id} step={step} />;
+				})}
+			</Container>
+		</Container>
+	);
+};
+
+export const AboutCard = ({ step }) => {
+	const classes = useStyles();
+	return (
+		<Card raised className={classes.fivei_card}>
+			<CardActionArea>
+				<CardMedia
+					className={classes.card_media}
+					image={step.image}
+					title={step.label}
+				/>
+				<CardContent>
+					<Typography gutterBottom variant="h5" component="h2">
+						{step.label}
+					</Typography>
+					<Typography variant="body2" color="textSecondary" component="p">
+						{step.info}
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+		</Card>
+	);
+};
