fbf9a17677c98e8904f82d165279ffb9282d423b
KAPIL <kapil992000@gmail.com>
2021-07-04 18:54:27 +0530

    added footer

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -8,6 +8,8 @@ import {
 	CardActionArea,
 	CardMedia,
 	CardContent,
+	Button,
+	Divider,
 } from "@material-ui/core";
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
 import KeyboardArrowLeftIcon from "@material-ui/icons/KeyboardArrowLeft";
@@ -63,90 +65,162 @@ const useStyles = makeStyles((theme) => ({
 		justifyContent: "center",
 		marginTop: theme.spacing(6),
 	},
+	footer: {
+		backgroundColor: "#d2c2fd",
+		padding: theme.spacing(4),
+	},
+	footer_top: {
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center",
+		[theme.breakpoints.up("sm")]: {
+			justifyContent: "space-between",
+		},
+		[theme.breakpoints.down("xs")]: {
+			flexDirection: "column",
+		},
+		marginBottom: theme.spacing(4),
+	},
+	copyright: {
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		marginTop: theme.spacing(4),
+		color: "grey",
+	},
 }));
 
 export const Home = () => {
 	const classes = useStyles();
 
 	return (
-		<Container disableGutters className={classes.home_page}>
-			<Container className={classes.homepage_top}>
-				<Grid xs={12} md={5} item className={classes.about_side}>
-					<Typography component="h1" variant="h3" style={{ color: "#5c68d6" }}>
-						About The Competiton
-					</Typography>
+		<>
+			<Container disableGutters className={classes.home_page}>
+				<Container className={classes.homepage_top}>
+					<Grid xs={12} md={5} item className={classes.about_side}>
+						<Typography
+							component="h1"
+							variant="h3"
+							style={{ color: "#5c68d6" }}
+						>
+							About The Competiton
+						</Typography>
+						<Typography
+							component="h3"
+							variant="h4"
+							style={{
+								color: "#d0007b",
+								fontWeight: "bold",
+								fontFamily: "Comic Sans MS",
+							}}
+						>
+							Ready Set Go!
+						</Typography>
+						<Typography
+							component="h6"
+							variant="h6"
+							style={{
+								color: "#000000",
+								fontFamily: "Comic Sans MS",
+								fontWeight: "bold",
+								marginTop: "25px",
+							}}
+						>
+							Our City, Our Challenge is inviting youth to demonstrate how your
+							act of Active Citizenship can potentially influence quality of
+							life.
+						</Typography>
+						<Typography
+							component="h6"
+							variant="h6"
+							style={{
+								color: "#434343",
+								marginTop: "25px",
+							}}
+						>
+							Active Citizenship actions by you, will go a long way in improving
+							our local communities, our cities and therefore, our country. The
+							Challenge enables 21st century skills in you! Share your
+							innovative ideas and actions with us!
+						</Typography>
+					</Grid>
+					<Grid xs={12} md={7} item className={classes.carousel_side}>
+						<Carousel
+							NextIcon={<KeyboardArrowRightIcon />}
+							PrevIcon={<KeyboardArrowLeftIcon />}
+						>
+							{carouselImages.map((cimg) => (
+								<img
+									key={cimg.id}
+									src={cimg.image}
+									className={classes.carousel_image}
+								/>
+							))}
+						</Carousel>
+					</Grid>
+				</Container>
+				<Grid item className={classes.fivei_heading}>
 					<Typography
 						component="h3"
 						variant="h4"
 						style={{
-							color: "#d0007b",
+							color: "#f186c0",
 							fontWeight: "bold",
 							fontFamily: "Comic Sans MS",
 						}}
 					>
-						Ready Set Go!
-					</Typography>
-					<Typography
-						component="h6"
-						variant="h6"
-						style={{
-							color: "#000000",
-							fontFamily: "Comic Sans MS",
-							fontWeight: "bold",
-							marginTop: "25px",
-						}}
-					>
-						Our City, Our Challenge is inviting youth to demonstrate how your
-						act of Active Citizenship can potentially influence quality of life.
-					</Typography>
-					<Typography
-						component="h6"
-						variant="h6"
-						style={{
-							color: "#434343",
-							marginTop: "25px",
-						}}
-					>
-						Active Citizenship actions by you, will go a long way in improving
-						our local communities, our cities and therefore, our country. The
-						Challenge enables 21st century skills in you! Share your innovative
-						ideas and actions with us!
+						5 Steps of the Challenge
 					</Typography>
 				</Grid>
-				<Grid xs={12} md={7} item className={classes.carousel_side}>
-					<Carousel
-						NextIcon={<KeyboardArrowRightIcon />}
-						PrevIcon={<KeyboardArrowLeftIcon />}
-					>
-						{carouselImages.map((cimg) => (
-							<img
-								key={cimg.id}
-								src={cimg.image}
-								className={classes.carousel_image}
-							/>
-						))}
-					</Carousel>
-				</Grid>
+				<Container className={classes.about_fivei}>
+					{fiveSteps.map((step) => {
+						return <AboutCard key={step.id} step={step} />;
+					})}
+				</Container>
 			</Container>
-			<Grid item className={classes.fivei_heading}>
-				<Typography
-					component="h3"
-					variant="h4"
-					style={{
-						color: "#f186c0",
-						fontWeight: "bold",
-						fontFamily: "Comic Sans MS",
-					}}
-				>
-					5 Steps of the Challenge
-				</Typography>
-			</Grid>
-			<Container className={classes.about_fivei}>
-				{fiveSteps.map((step) => {
-					return <AboutCard key={step.id} step={step} />;
-				})}
-			</Container>
-		</Container>
+			<div className={classes.footer}>
+				<Container disableGutters className={classes.footer_box}>
+					<Container className={classes.footer_top}>
+						<Grid item>
+							<Button
+								variant="contained"
+								color="primary"
+								className={classes.guideline}
+								href="https://challenge.balajanaagraha.org/guidelines"
+								target="blank"
+								style={{padding: "20px"}}
+							>
+								Download Project Guidelines
+							</Button>
+						</Grid>
+						<Grid item>
+							<Typography>Want to know more?</Typography>
+							<Typography>
+								Whatsapp us on{" "}
+								<span style={{ fontWeight: "bold" }}>9591311293</span>
+							</Typography>
+							<Typography>
+								Call us on{" "}
+								<span style={{ fontWeight: "bold" }}>9591311293</span>
+							</Typography>
+							<Typography>
+								Or send us an email at{" "}
+								<span style={{ fontWeight: "bold" }}>
+									balajana @janaagraha.org
+								</span>
+							</Typography>
+						</Grid>
+					</Container>
+					<Divider variant="fullWidth"></Divider>
+					<Grid item xs={12} className={classes.copyright}>
+						<Typography>
+							Copyright 2020 Janaagraha Centre for Citizenship & Democracy |
+							Privacy Policy | Terms & Conditions
+						</Typography>
+					</Grid>
+				</Container>
+			</div>
+		</>
 	);
 };
 
